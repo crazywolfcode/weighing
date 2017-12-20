8ae22d6c1525b20762fd66a58976bd9b960e9a06
hacker <hacker@DESKTOP-KEFM18T>
2017-12-20 10:41:11 +0800

    Revert "项目初始化"
    This reverts commit 05435086ff0b8dc98716c21aff6323b159108fe3.

---
+++ application/common/controller/CommonBase.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\index\controller;
+
+/**
+ * Description of CommonBase
+ *
+ * @author hacker
+ * @QQ 443055589
+ * @DateTime 2017-12-20 9:50:21 
+ */
+class CommonBase {
+    //put your code here
+}
---
+++ application/home.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    database => [
+        // 数据库类型
+        'type' => 'mysql',
+        // 服务器地址
+        'hostname' => '180.130.175.254',
+        // 数据库名
+        'database' => 'weighing',
+        // 用户名
+        'username' => 'admin',
+        // 密码
+        'password' => 'Txmy0071',
+        // 端口
+        'hostport' => '33061',
+        // 连接dsn
+        'dsn' => '',
+        // 数据库连接参数
+        'params' => [],
+    ],
+];
